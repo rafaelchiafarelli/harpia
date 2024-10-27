406b54dec2cd94ed5fed35a6a9f62001be355659
Rafael Toschi Chiafarelli <rafael.chiafarelli@gmail.com>
2024-10-27 12:46:14 -0300

    general organization added the HowToUse file

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 ##this is a file meant to be executed as the main executor
 import os
 from LexicalAnalizer.LexicalAnalyzer import LexicalAnalyzer
-from logger.logger import logger
+from Logger.logger import logger
 from LexicalAnalizer.pre_lex import pre_lex
 from LexicalAnalizer.Remover import Remover
 from LexicalAnalizer.MessageCreator import MessageCreator,Message
-from protoFile.ProtoFileProcessor import ProtoFileProcessor
-from protoFile.FileCreator import FileCreator
+from ProtoFile.ProtoFileProcessor import ProtoFileProcessor
+from ProtoFile.FileCreator import FileCreator
 if __name__ == '__main__':
-    print("Path at terminal when executing this file")
-    print(os.getcwd() + "\n")
-    print("This file path, relative to os.getcwd()")
-    print(__file__ + "\n")
-
-    print("This file full path (following symlinks)")
-    full_path = os.path.realpath(__file__)
-    print(full_path + "\n")
-
-    print("This file directory and name")
-    path, filename = os.path.split(full_path)
-    print(path + ' --> ' + filename + "\n")
-
-    print("This file directory only")
-    print(os.path.dirname(full_path))
-    local_folder = os.path.dirname(full_path)
     log = logger(outFile=None, moduleName="main" )
-    preProcessor = pre_lex(folders=[local_folder], file="test.harpia")
+    log.print("Path at terminal when executing this file")
+    log.print(os.getcwd() + "\n")
+    log.print("This file path, relative to os.getcwd()")
+    log.print(__file__ + "\n")
+
+    log.print("This file full path (following symlinks)")
+    full_path = os.path.realpath(__file__)
+    log.print(full_path + "\n")
+
+    log.print("This file directory and name")
+    path, filename = os.path.split(full_path)
+    log.print(path + ' --> ' + filename + "\n")
+
+    log.print("This file directory only")
+    log.print(os.path.dirname(full_path))
+
+    local_folder = os.path.dirname(full_path)
+    testFile = "./Test/test.harpia"
+    preProcessor = pre_lex(folders=[local_folder], file=testFile,dest="./build")
     preProcessorResult = preProcessor.process()
 
     if preProcessorResult is None: ##no error detected
         analizer = LexicalAnalyzer()
 
-        with open(local_folder+"/test.harpia","r") as inFile:
+        with open(local_folder+testFile,"r") as inFile:
             while True:
                 line = inFile.readline()
                 if not line:
@@ -51,7 +53,7 @@ if __name__ == '__main__':
                 log.print(messages.__str__())
                 exit(-1)
             for msg in msgFactory.messages:
-                fileCreator = FileCreator(message=msg,imports=imports )
+                fileCreator = FileCreator(message=msg,imports=imports , dest="./build")
                 fileCreator.Process()
                 fileCreator.save()
             #log.print(msgFactory.__str__())
